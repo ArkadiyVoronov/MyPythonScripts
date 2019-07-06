4335981ba16adc03afbd4f00b694eb96eaf49cd8
Arkadiy Voronov <voronov_ao@mail.ru>
2019-07-07 04:44:34 +0500

    add fix
    linter PEP8

--- filesForBook/interface4.py
+++ filesForBook/interface4.py
@@ -2,11 +2,11 @@
 
 
 def get_networks(gateways_dict):
-	networks_dict = {}
-	for key, value in gateways.iteritems():
-		gateway_ip, iface = value[0], value[1]
-		hwaddress, addr, broadcast, netmask = get_addresses(iface)
-		network = {'gateway': gateway_ip, 'hwaddr': hwaddress,
-		'addr': addr, 'broadcast': broadcast, 'netmask': netmask}
-		networks_dict[iface] = network
-	return networks_dict
+        networks_dict = {}
+        for key, value in gateways.iteritems():
+                gateway_ip, iface = value[0], value[1]
+                hwaddress, addr, broadcast, netmask = get_addresses(iface)
+                network = {'gateway': gateway_ip, 'hwaddr': hwaddress,
+                'addr': addr, 'broadcast': broadcast, 'netmask': netmask}
+                networks_dict[iface] = network
+        return networks_dict
