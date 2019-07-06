861e44133076cca4a33f9a7804a553f7bc99e09d
Arkadiy Voronov <voronov_ao@mail.ru>
2019-07-07 03:49:59 +0500

    add fix
    linter PEP8

--- filesForBook/arguments1.py
+++ filesForBook/arguments1.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 import sys
 
 arguments = sys.argv
 print("The number of arguments passed was: %s") % (str(len(arguments)))
-i=0
+i = 0
 for x in arguments:
-    print("The %d argument is %s") % (i,x)
-    i+=1
+    print("The %d argument is %s") % (i, x)
+    i += 1
