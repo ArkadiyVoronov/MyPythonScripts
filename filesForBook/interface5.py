98e4f479890f73e1b5d5c570ff1fae1206b77051
Arkadiy Voronov <voronov_ao@mail.ru>
2019-07-07 04:46:05 +0500

    add fix

--- filesForBook/interface5.py
+++ filesForBook/interface5.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
+
 
 def get_networks(gateways_dict):
 	networks_dict = {}
 	for key, value in gateways.iteritems():
 		gateway_ip, iface = value[0], value[1]
 		hwaddress, addr, broadcast, netmask = get_addresses(iface)
-		network = {'gateway': gateway_ip, 'hwaddr' : hwaddress,
-		'addr' : addr, 'broadcast' : broadcast, 'netmask' : netmask}
+		network = {'gateway': gateway_ip, 'hwaddr': hwaddress,
+		'addr': addr, 'broadcast': broadcast, 'netmask': netmask}
 		networks_dict[iface] = network
 	return networks_dict
