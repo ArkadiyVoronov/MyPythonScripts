027666c5dc6e524059541c4d0e38f4733d6c4cc4
Arkadiy Voronov <voronov_ao@mail.ru>
2019-07-08 23:11:46 +0500

    add fix

--- filesForBook/string2.py
+++ filesForBook/string2.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 
-list_example = [100,222,333,444]
+
+list_example = [100, 222, 333, 444]
 list_value = list_example[2]
 string_value_from_list = str(list_value)
 print("String value from list: %s") % (str(list_value))
